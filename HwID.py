375da910ce167d33c970a443e0673068c297e6cf
LaciaRD000 <radonxenon001@gmail.com>
2023-04-02 16:49:45 +0900

    Update HwID.py

--- HwID.py
+++ HwID.py
@@ -1,10 +1,10 @@
-import json
-
-auth = json.loads(
-    """{
+{
     "D5C245E7-E8FB-11EA-8B14-C245E6C59A06": {
         "username": "Radon",
         "rank": "ADMIN"
     },
-}"""
-)
+    "114514-E8FB-11EA-8B14-810": {
+        "username": "あうあう",
+        "rank": "BUYER"
+    }
+}
